26bc8636cdfb22efc184900f3209129c736081e0
Marcos Henrique <marcosmarcone48@gmail.com>
2022-08-12 23:41:33 -0300

    Initial Configuration

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+
+import app from "./app.js";
+import logging from "./utils/logging.js";
+
+dotenv.config();
+
+const PORT = +process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(logging.info(`The server is running on PORT ${PORT}...`));
+});
