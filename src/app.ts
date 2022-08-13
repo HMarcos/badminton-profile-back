68086a29e8ede74a4a2029c471321f0a6b5fd256
Marcos Henrique <marcosmarcone48@gmail.com>
2022-08-13 00:56:08 -0300

    FEAT: add handle errors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { json } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import handleErrors from './middlewares/handleErrorsMiddleware.js';
 
 const app = express();
 app.use(cors());
 app.use(json());
+app.use(handleErrors);
 
 export default app;
